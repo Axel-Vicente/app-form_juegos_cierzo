56257162a8546ce41ba2e9e46186773bcc17ce3d
Axel <axelyleya2011@gmail.com>
2023-02-22 23:39:15 +0100

    Added assets and main App/index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react"
+import React from "react";
+
+import { Article, Brand, CTA, Feature, Navbar } from "./components";
+import { Blog, Header } from "./containers";
+import './App.css'
 
 const App = () => {
-    return ( <
-        div >
-        <
-        h1 > Juegos del cierzo < /h1> <
-        /div>
+    return (
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+        </div>
     )
 }
 
