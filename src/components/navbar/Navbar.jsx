11ecbec1864095ef298bfe15776d7ab69d0283cb
Axel <axelyleya2011@gmail.com>
2023-02-23 13:11:56 +0100

    Added Navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import './navbar.css';
+import logo from '../../assets/exampleLogo.jfif'
+
+const Menu = () => (
+  <>
+    <p><a href='#home'>home</a></p>
+    <p><a href='#home'>home</a></p>
+    <p><a href='#home'>home</a></p>
+    <p><a href='#home'>home</a></p>
+  </>
+)
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <div className='cierzo-games__navbar'>
+      <div className='cierzo-games__navbar-links'>
+        <div className='cierzo-games__navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+        <div className='cierzo-games__navbar-links_container'>
+          <Menu />
+        </div>
+      </div>
+      <div className='cierzo-games__navbar-sign'>
+        <button type='button'>iniciar sesion</button>
+      </div>
+      <div className='cierzo-games__navbar-menu'>
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className='cierzo-games__navbar-menu_container scale-up-center'>
+            <div className='cierzo-games__navbar-menu_container-links'>
+              <Menu />
+            </div>
+              <div className='cierzo-games__navbar-menu_container-links-sign'>
+                <button type='button'>iniciar sesion</button>
+              </div>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
